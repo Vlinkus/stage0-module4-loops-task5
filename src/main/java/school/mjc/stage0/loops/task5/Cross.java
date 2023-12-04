0acdb8072c7757363d399365fb480b1991843800
VLinkus <LinkusVyte@gmail.com>
2023-12-04 13:08:18 +0200

    M4 Task Loops5 Completed

--- src/main/java/school/mjc/stage0/loops/task5/Cross.java
+++ src/main/java/school/mjc/stage0/loops/task5/Cross.java
@@ -2,6 +2,16 @@ package school.mjc.stage0.loops.task5;
 
 public class Cross {
     public void printCross(int sideLength) {
-
+        int middle = sideLength%2 == 0? sideLength/2 : sideLength/2+1;
+        for (int i = 1; i <= sideLength ; i++) {
+            for (int j = 1; j <= sideLength; j++) {
+                if(i == middle || j == middle){
+                    System.out.print(8);
+                } else{
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
+++ src/main/java/school/mjc/stage0/loops/task5/Hourglass.java
@@ -2,6 +2,22 @@ package school.mjc.stage0.loops.task5;
 
 public class Hourglass {
     public void printHourglassOfGivenSize(int height) {
-
+        int middle = height%2 == 0? height/2 : height/2+1;
+        for (int i = 1; i <= height; i++) {
+            for (int j = 1; j <= height ; j++) {
+                if( i == 1 || i == height ){
+                    System.out.print(8);
+                } else if ( (i > j && j <= height-i)){
+                    System.out.print(" ");
+                } else if( i > middle && j > middle && i < j){
+                    System.out.print(" ");
+                } else if( i <= middle && j > middle && (height-(i-1)) < j){
+                    System.out.print(" ");
+                } else {
+                    System.out.print(8);
+                }
+            }
+            System.out.println();
+        }
     }
 }
